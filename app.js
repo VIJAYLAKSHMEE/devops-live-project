0efc7d0505f77a396ba1d3c3b9920f5f0fb3e64f
VIJAYLAKSHMEE <vijuharishetter@gmail.com>
2026-02-13 12:39:46 +0530

    text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Live Project - Version 3 🚀');
+  res.send('DevOps Live Project - Version 1 🚀');
 });
 
 app.listen(3000, () => {
